8f3c2f3edb432733203851aa38ca5a86a44d3baf
Specter242 <aaroneast@tlacil.com>
2025-02-25 16:49:34 -0500

    cleaninput

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	for {
+		fmt.Print("Pokedex > ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		text := scanner.Text()
+		words := cleanInput(text)
+		firstWord := words[0]
+		fmt.Printf("Your command was: %s", firstWord)
+	}
+}
+
+func cleanInput(text string) []string {
+	// Remove any leading or trailing whitespace
+	text = strings.TrimSpace(text)
+
+	// Split the input text into words
+	words := strings.Fields(text)
+
+	// Create output slice with correct capacity
+	cleanOutput := make([]string, len(words))
+
+	// Lowercase the words and remove any commas
+	for i, word := range words {
+		word = strings.ToLower(word)
+		word = strings.Trim(word, ",")
+		cleanOutput[i] = word
+	}
+
+	return cleanOutput
 }
---
+++ repl_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Hello, world  ",
+			expected: []string{"hello", "world"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("Expected %d words, but got %d", len(c.expected), len(actual))
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+			if word != expectedWord {
+				t.Errorf("Expected word %q, but got %q", expectedWord, word)
+			}
+		}
+	}
+}
